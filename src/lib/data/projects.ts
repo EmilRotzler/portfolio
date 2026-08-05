683bda606c5544c18ca408d057daf31f969ae0af
EmilRotzler <emilarotzler@hotmail.com>
2026-08-05 13:17:17 +0200

    Cut out old projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -42,42 +42,10 @@ export const projects: Project[] = [
 		tags: ['React', 'Firebase'],
 	},
 	{
-		title: 'Custom Data Copy',
-		description:
-			'Tool to copy custom data text from one language onto the others for the same custom data entry.',
-		url: 'https://github.com/EmilRotzler/CustomDataCopySW',
-		tags: ['Utility'],
-	},
-	{
-		title: 'Tic Tac Toe',
-		description: 'Simple ReactJS tic-tac-toe game running in the browser.',
-		url: 'https://github.com/EmilRotzler/tic-tac-toe-ReactJS',
-		tags: ['React'],
-	},
-	{
-		title: 'Tour of Heroes',
-		description:
-			'Angular content listing and navigation project based on the official Angular tutorial.',
-		url: 'https://github.com/EmilRotzler/TourOfHeroes-Angular',
-		tags: ['Angular', 'TypeScript'],
-	},
-	{
 		title: 'Product Translate',
 		description:
 			'Uses the Google Translate API to automatically translate product titles in the Smartweb CMS.',
 		url: 'https://github.com/EmilRotzler/ProductTranslateSmartweb',
 		tags: ['Google API', 'CMS'],
-	},
-	{
-		title: 'Coin Collector',
-		description: 'Android game project.',
-		url: 'https://github.com/EmilRotzler/CoinCollector',
-		tags: ['Android', 'Java'],
-	},
-	{
-		title: 'Shopping List',
-		description: 'Android shopping list app backed by Google Firebase for real-time sync.',
-		url: 'https://github.com/EmilRotzler/ShoppingList-Master',
-		tags: ['Android', 'Firebase'],
 	},
 ];
